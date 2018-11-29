ef5639246aa675e669bc9ee13463f3de50387636
Niklas Mayer <niklas.mayer@crealogix.com>
2018-11-29 16:21:46 +0100

    Create Data Mutation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import{
     GraphQLServer
 } from "graphql-yoga";
 
+import uuidv4 from 'uuid/v4'
+
 // String, Boolean, Int, Float, ID
 //Demo user data
 const users = [{
@@ -88,6 +90,12 @@ const comments = [{
         comments: [Comment!]!
         }
         
+        type Mutation {
+           createUser(name: String!, email: String!, age: Int): User!
+           createPost(title: String!, body: String!, published: Boolean!, author: ID! ): Post!
+           createComment(text: String, author: ID!, post: ID!): Comment!
+        }
+        
         type User {
         id: ID!
         name: String!
@@ -154,6 +162,68 @@ const comments = [{
                 return comments
             }
         },
+        Mutation: {
+            createUser(parent, args, ctx, info) {
+                const emailTaken = users.some((user) => {
+                    return user.email === args.email
+                });
+                if(emailTaken) {
+                    throw new Error('Email taken.')
+                }
+                const user = {
+                    id: uuidv4(),
+                    name: args.name,
+                    email: args.email,
+                    age: args.age
+                };
+
+                users.push(user);
+                return user
+            },
+            createPost(parent, args, ctx, info) {
+                const authorExists = users.some((user) => {
+                    return user.id === args.author
+                });
+
+                if(!authorExists) {
+                    throw new Error('author does not exist.')
+                }
+                const post = {
+                    id: uuidv4(),
+                    title: args.title,
+                    body: args.body,
+                    published: args.published,
+                    author: args.author
+                };
+
+                posts.push(post);
+                return post
+            },
+            createComment(parent, args, ctx, info) {
+                const authorExists = users.some((user) => {
+                    return user.id === args.author
+                });
+
+                const postExists = posts.some((post) => {
+                    return post.id === args.post && post.published === true
+                });
+
+                if(!authorExists || !postExists) {
+                    throw new Error('author or post does not exist, or is not published yet')
+                }
+
+
+                const comment = {
+                    id: uuidv4(),
+                    text: args.text,
+                    author: args.author,
+                    post: args.post
+                };
+
+                comments.push(comment);
+                return comment
+            }
+        },
         Post:{
             author(parent, args, ctx, info) {
                 return users.find((user) => {
@@ -190,6 +260,7 @@ const comments = [{
                 });
             }
         }
+
     };
 
     const server = new GraphQLServer({
